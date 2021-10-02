f8ec210bce4699e5ea58c53d9353080511fdee59
Kieu Nguyen <Q@Kieus-MacBook-Air.local>
2021-10-02 12:19:38 -0400

    successful data pulls

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const apiKey = "70b920f95932ae4fe546e834220435d2"
-const privateKey = "0a5d5403a5fd734ebffc0e8b91d6c9d71b3efb22"
+//Marvel Keys
+// const apiKey = "70b920f95932ae4fe546e834220435d2"
+// const privateKey = "0a5d5403a5fd734ebffc0e8b91d6c9d71b3efb22"
+//GOT website href "https://gameofthronesquotes.xyz/""
 
-data: {"limit" : 3000}
-//your rate limit is 3000 calls/day
 $(() => {
+   $
+
 
    $.ajax(
       {
-         url: `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=70b920f95932ae4fe546e834220435d2`
+         url: `https://game-of-thrones-quotes.herokuapp.com/v1/houses`,
+
       }
    ).then(
       (data) => {
          console.log(data);
+         //logs an array of all the houses and all the members
+         console.log(data[0].members);
+         //access house with index 0 and the members in that house. Starks
       },
       () => {
          console.log("bad request");
