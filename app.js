d4866910caecac61518b4167549da49df2793d96
Kieu Nguyen <Q@Kieus-MacBook-Air.local>
2021-10-04 13:11:51 -0400

    MVP done

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
+let membersIndex = 0
+
 $(() => {
+   $('.sigil').on('click', (event) => {
+      $(event.currentTarget).children().eq(1).removeClass('hide');
+
+      let name = $(event.target).val()
+      console.log(name);
+
    const $modal = $('.modal');
 
    const $closeButton = $('.close');
 
-   const openModal = () => {
-      $modal.removeClass('hide');
-   }
+   // const openModal = () => {
+   //    $modal.removeClass('hide');
+   // }
 
    const closeModal = () => {
       // console.log($(event.currentTarget).parent().parent());
       $modal.addClass('hide');
       return false;
+      //stops code from bubbling up and running openModal function.
    }
 
-   $('.sigil').on('click', openModal);
+
    //when they click the sigil, open the modal.
 
    $closeButton.on('click', closeModal);
 
+
    $.ajax(
       {
-         url: `https://game-of-thrones-quotes.herokuapp.com/v1/houses`,
+         url: `https://game-of-thrones-quotes.herokuapp.com/v1/house/${name}`,
 
       }
    ).then(
       (data) => {
          $('.text').remove()
          //clears the modal
-         // console.log(data[0].members[0].name);
-         // const $p = $('<p>').addClass('text').text(data[0].members[0].name).appendTo('.modal-textbox')
 
          for (let info of data){
-            console.log(info.name);
-            const $p = $('<p>').addClass('text').text(info.name).appendTo('.modal-textbox')
+
+            // if (membersIndex < info.members.length -1){
+            //    membersIndex++
+            for (i=0; i<info.members.length; i++){
+            console.log(info.members[i].name);
+            const $p = $('<p>').addClass('text').text(info.members[i].name).appendTo('.modal-textbox')
          }
-
-
+      }
 
          console.log(data);
          //logs an array of all the houses and all the members
-         // console.log(data.name);
-         // console.log(data[0].members);
+         // console.log(data[0]);
          // access house with index 0 and the members in that house. Starks
+
       },
       () => {
          console.log("bad request");
       }
    )
+   });
 })
 
 
